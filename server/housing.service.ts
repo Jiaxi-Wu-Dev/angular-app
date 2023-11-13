07cc9905ab288c4785fcac062eefd03018854f17
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-11-13 16:45:56 -0600

    moved service file into server

--- server/housing.service.ts
+++ server/housing.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HousingLocation } from './housinglocation';
+import { HousingLocation } from '../src/app/housinglocation';
 
 @Injectable({
   providedIn: 'root',
